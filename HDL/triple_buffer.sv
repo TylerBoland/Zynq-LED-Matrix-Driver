9e3def202050951f5c3da8608eeffa677e171a05
TylerBoland <96139852+TylerBoland@users.noreply.github.com>
2021-12-16 19:31:03 -0600

    Update triple_buffer.sv

--- HDL/triple_buffer.sv
+++ HDL/triple_buffer.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date: 12/16/2021 12:06:57 PM
-// Design Name:
-// Module Name: triple_buffer
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module triple_buffer(
     input wire resetn,
