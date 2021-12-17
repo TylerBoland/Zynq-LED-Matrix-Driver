3f15ac295e58254b9f31a24c2d7441c2eb1c1fee
TylerBoland <96139852+TylerBoland@users.noreply.github.com>
2021-12-16 19:30:12 -0600

    Update gamma_correction.sv

--- HDL/gamma_correction.sv
+++ HDL/gamma_correction.sv
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
-//
-// Create Date: 12/16/2021 11:08:15 AM
-// Design Name:
-// Module Name: gamma_correction
-// Project Name:
-// Target Devices:
-// Tool Versions:
-// Description:
-//
-// Dependencies:
-//
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-//
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module gamma_correction(
     input logic [23:0] din,
